7960ca97035160077be24ea7f29665eabc010fdc
ten9miq <ruin.of.messger@gmail.com>
2026-09-06 14:33:24 +0900

    security: remove redundant host permissions

--- tests/manifest.test.js
+++ tests/manifest.test.js
@@ -21,10 +21,11 @@ function contrastRatio(foreground, background) {
 
 test('manifest identifies the navigator and only stores extension preferences', () => {
   assert.equal(manifest.name, 'Prompt Outline Navigator');
-  assert.equal(manifest.version, '1.3.28');
+  assert.equal(manifest.version, '1.3.29');
   assert.equal(manifest.author, 'ten9miq');
   assert.equal(packageJson.author, 'ten9miq');
   assert.deepEqual(manifest.permissions, ['storage']);
+  assert.equal(manifest.host_permissions, undefined);
   assert.equal(manifest.key, undefined);
   assert.equal(manifest.update_url, undefined);
 });
